da9806ca24cb280784eab6f0d93f1abed73faf93
Pavel Sucharev <pawelsucharew@gmail.com>
2020-08-27 12:34:09 +0300

    basic back-end created, auth route created

---
+++ routes/auth.routes.js
@@ -0,0 +1,57 @@
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const router = Router();
+
+// /api/auth/register
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const candidate = await User.findOne({ email });
+
+    if (candidate) {
+      return res.status(400).json({ message: "User already exist" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({ email, password: hashedPassword });
+
+    await user.save();
+
+    res.status(201).json({ message: "User created" });
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong :( try again." });
+  }
+});
+
+// /api/auth/login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "password incorrect" });
+    }
+
+    const token = jwt.sign({ userID: user.id }, config.get("jwtSecret"), {
+      expiresIn: "1h",
+    });
+
+    res.json({ token, userId: user.id });
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong :( try again." });
+  }
+});
+
+module.exports = router;
